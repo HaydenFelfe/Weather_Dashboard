f5b6ffc2c92caf6da01c5cdb134e0a42b68f8749
haydenfelfe <felfehayden@gmail.com>
2023-09-06 14:23:39 -0700

    Added in global variables and dom element references

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var searchHistory = [];
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+var weatherApiKey = '';
+
+
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var todayContainer = document.querySelector('#today');
+var forecastContainer = document.querySelector('#forecast');
+var searchHistoryContainer = document.querySelector('#history');
